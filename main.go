c45b1ffd405abeccacd92f1d40770c104eee74a6
Marcel <marcelkaemper862@gmail.com>
2019-07-13 00:51:52 +0200

    Reading sudoku file letter by letter

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import(
 	"fmt"
+	"io/ioutil"
 )
 
 func main() {
-	fmt.Println("")
+	dat,err := ioutil.ReadFile("sudoku")
+	if err != nil {
+		panic("File not found")
+	}
+
+	for i := 0; i < 9; i++{
+		for j := 0; j < 9; j++{
+			cell := string(dat[j+10*i])
+			if cell != "\n" {
+				fmt.Print(cell)
+			}
+			// fmt.Print(i,j)
+		}
+		fmt.Print("\n")
+	}
 }
